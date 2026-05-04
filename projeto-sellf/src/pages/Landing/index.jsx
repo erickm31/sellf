b7a74a2ab8c49994dcb5e82153269a113f6b6158
JoaoBozz <163414633+JoaoBozz@users.noreply.github.com>
2026-05-04 19:47:16 -0300

    Atualizacao landingpage

--- projeto-sellf/src/pages/Landing/index.jsx
+++ projeto-sellf/src/pages/Landing/index.jsx
@@ -3,7 +3,7 @@ import styles from "./styles.module.css";
 import Button from "../../components/ui/Button";
 import Header from "../../components/layout/Header";
 import Footer from "../../components/layout/Footer";
-import sellfazul from "../../assets/sellfazul.png";
+import sellfazul from "../../assets/sellf.png";
 import sellimg from "../../assets/imgSellfteste.png";
 
 export default function Landing() {
@@ -12,7 +12,7 @@ export default function Landing() {
   return (
     <div className={styles.container}>
       <Header></Header>
-        <section className={`${styles.section} ${styles.section1}`}>
+        <section className={`${styles.section1}`}>
           <div className={styles.esquerda}>
             <h1 className={styles.title}>A vitrine digital da sua vizinhança! <span className={styles.subtitle}>Compre e Venda localmente com o Sellf</span></h1>
             <p className={styles.p}>
@@ -26,11 +26,27 @@ export default function Landing() {
             <img className={styles.img} src={sellimg} alt="Sellf" />
           </div>
         </section>
-        <section className={`${styles.section} ${styles.section2}`}>
-
+        <section className={`${styles.section2}`}>
+          <div className={styles.left}>
+            <img className={styles.sellf} src={sellfazul} alt="Sellf Azul" />
+          </div>
+          <div className={styles.right}>
+            <div className={styles.card}>
+              <h1 className={styles.cardTitle}>+ 1k</h1>
+              <h3 className={styles.cardSubtitle}>Vendedores ativos</h3>
+            </div>
+            <div className={styles.card}>
+              <h1 className={styles.cardTitle}>+ 200</h1>
+              <h3 className={styles.cardSubtitle}>Compras realizadas</h3>
+            </div>
+            <div className={styles.card}>
+              <h1 className={styles.cardTitle}>+100</h1>
+              <h3 className={styles.cardSubtitle}>Produtos anunciados</h3>
+            </div>
+          </div>
         </section>
-        <section className={`${styles.section} ${styles.section3}`}>
-
+        <section className={`${styles.section3}`}>
+          
         </section>
       <Footer></Footer>
     </div>
